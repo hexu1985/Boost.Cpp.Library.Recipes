5df2b3e68c4c66109ef2c7e0ae72ef4e524fbbe1
hexu <hexu_1985@sina.com>
2025-05-17 09:13:12 +0800

    add udp/recipe-01/linux

---
+++ udp/recipe-01/linux/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+cd $(dirname ${BASH_SOURCE[0]})
+
+./uecho_server 9999 &
+SERVER_PID=$!
+
+sleep 1
+
+./uecho_client 127.0.0.1 9999 <<EOF
+Hi UDP Server?
+Nice to meet you!
+Good bye~
+q
+EOF
+
+./uecho_con_client 127.0.0.1 9999 <<EOF
+Hi UDP Server?
+Nice to meet you!
+Good bye~
+q
+EOF
+
+kill -9 ${SERVER_PID}
