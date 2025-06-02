7bdcaa3e5571fbc57d1310f4a8750064eb164904
hexu <hexu_1985@sina.com>
2025-06-02 21:59:43 +0800

    update udp/recipe-03/linux

--- udp/recipe-03/linux/news_sender.c
+++ udp/recipe-03/linux/news_sender.c
@@ -10,40 +10,40 @@ void error_handling(char *message);
 
 int main(int argc, char *argv[])
 {
-	int send_sock;
-	struct sockaddr_in remote_addr;
-	FILE *fp;
-	char buf[BUF_SIZE];
-	
-	if(argc!=3) {
-		printf("Usage : %s <IP> <PORT>\n", argv[0]);
-		exit(1);
-	}
-  
-	send_sock=socket(PF_INET, SOCK_DGRAM, 0);	
-	memset(&remote_addr, 0, sizeof(remote_addr));
-	remote_addr.sin_family=AF_INET;
-	remote_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	remote_addr.sin_port=htons(atoi(argv[2]));
-	
-	if((fp=fopen("news.txt", "r"))==NULL)
-		error_handling("fopen() error");
+    int send_sock;
+    struct sockaddr_in remote_addr;
+    FILE *fp;
+    char buf[BUF_SIZE];
 
-	while(!feof(fp))
-	{
-		fgets(buf, BUF_SIZE, fp);
-		sendto(send_sock, buf, strlen(buf), 
-			0, (struct sockaddr*)&remote_addr, sizeof(remote_addr));
-		sleep(2);
-	}
+    if(argc!=3) {
+        printf("Usage : %s <IP> <PORT>\n", argv[0]);
+        exit(1);
+    }
 
-	close(send_sock);
-	return 0;
+    send_sock=socket(PF_INET, SOCK_DGRAM, 0);	
+    memset(&remote_addr, 0, sizeof(remote_addr));
+    remote_addr.sin_family=AF_INET;
+    remote_addr.sin_addr.s_addr=inet_addr(argv[1]);
+    remote_addr.sin_port=htons(atoi(argv[2]));
+
+    if((fp=fopen("news.txt", "r"))==NULL)
+        error_handling("fopen() error");
+
+    while(!feof(fp))
+    {
+        fgets(buf, BUF_SIZE, fp);
+        sendto(send_sock, buf, strlen(buf), 
+                0, (struct sockaddr*)&remote_addr, sizeof(remote_addr));
+        sleep(2);
+    }
+
+    close(send_sock);
+    return 0;
 }
 
 void error_handling(char *message)
 {
-	fputs(message, stderr);
-	fputc('\n', stderr);
-	exit(1);
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
 }
