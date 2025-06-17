2dc7f2f399f781d3ee40dd2b242ba8982cc39703
hexu <hexu_1985@sina.com>
2025-06-17 19:31:59 +0800

    update filesystem/recipe-01/boost

--- filesystem/recipe-01/boost/build_by_cmake.sh
+++ filesystem/recipe-01/boost/build_by_cmake.sh
@@ -4,7 +4,7 @@ current_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
 src_dir=${current_dir}
 build_dir=${current_dir}/build
 
-cmake -S ${src_dir} -B ${build_dir}
+cmake -S ${src_dir} -B ${build_dir} -DBoost_DIR=~/local/boost/lib/cmake/Boost-1.88.0
 cmake --build ${build_dir} -j 4
 
 
