e7ef5039ad52531f2167f99373559fbab06aac98
hexu <hexu_1985@sina.com>
2025-06-18 19:35:44 +0800

    update semaphores

--- semaphores/recipe-01/boost/build_by_cmake.sh
+++ semaphores/recipe-01/boost/build_by_cmake.sh
@@ -4,6 +4,8 @@ current_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
 src_dir=${current_dir}
 build_dir=${current_dir}/build
 
-cmake -S ${src_dir} -B ${build_dir} -DCMAKE_BUILD_TYPE=Debug
+cmake -S ${src_dir} -B ${build_dir} \
+    -DBoost_DIR=~/local/boost/lib/cmake/Boost-1.88.0 \
+    -DCMAKE_BUILD_TYPE=Debug
 cmake --build ${build_dir} -j 4
 
