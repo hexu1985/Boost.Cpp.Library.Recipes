0a7cf3d750adf4e2c708bed0b7406d69e3c13221
hexu <hexu_1985@sina.com>
2025-06-19 14:43:36 +0800

    update stacktrace/recipe-01/boost

--- stacktrace/recipe-01/boost/build_by_cmake.sh
+++ stacktrace/recipe-01/boost/build_by_cmake.sh
@@ -4,7 +4,9 @@ current_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
 src_dir=${current_dir}
 build_dir=${current_dir}/build
 
-cmake -S ${src_dir} -B ${build_dir} -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake -S ${src_dir} -B ${build_dir} \
+    -DBoost_DIR=~/local/boost/lib/cmake/Boost-1.88.0 \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo
 cmake --build ${build_dir} -j 4
 
 
