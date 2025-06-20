3a95d2ed3e909792d45620a0340cafa5300b591d
hexu <hexu_1985@sina.com>
2025-06-20 19:08:49 +0800

    add intrusive_ptr/recipe-01/boost

---
+++ intrusive_ptr/recipe-01/boost/main.cpp
@@ -0,0 +1,39 @@
+#include "ProcessData.h"
+#include <boost/intrusive_ptr.hpp>
+
+int main()
+{
+    boost::intrusive_ptr<ProcessData> ptr(new ProcessData(1, "a"));
+    std::cout << "******************"<< std::endl;
+    std::cout << "ref_count = " << ptr->RefCount() << std::endl;
+    std::cout << "******************" << std::endl;
+    {
+        boost::intrusive_ptr<ProcessData> ptrCopy(ptr.get());
+        std::cout << "ref_count after copy constructed = " << ptrCopy->RefCount() << std::endl;
+    }
+
+    std::cout << "******************" << std::endl;
+    std::cout << "ref_count = " << ptr->RefCount() << std::endl;
+    std::cout << "******************" << std::endl;
+
+    {
+        boost::intrusive_ptr<ProcessData> ptrAssignment = ptr;
+        std::cout << "ref_count after assignment = " << ptrAssignment->RefCount() << std::endl;
+    }
+
+    std::cout << "******************" << std::endl;
+    std::cout << "ref_count = " << ptr->RefCount() << std::endl;
+    std::cout << "******************" << std::endl;
+
+    {
+        boost::intrusive_ptr<ProcessData> ptrWeak(ptr.get(), false);
+        std::cout << "ref_count after construct weak_ptr = " << ptrWeak->RefCount() << std::endl;
+    }
+
+    std::cout << "******************" << std::endl;
+    std::cout << "ref_count = " << ptr->RefCount() << std::endl;
+    std::cout << "******************" << std::endl;
+    
+    return 0;
+}
+
