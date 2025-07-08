96d3eef14de80b15f1191cab1bca7f1262082c99
hexu <hexu_1985@sina.com>
2025-07-08 19:37:39 +0800

    update timer/recipe-01/my_impl: 完成Timer所有接口实现，编译通过

--- timer/recipe-01/my_impl/lib/Timer.cpp
+++ timer/recipe-01/my_impl/lib/Timer.cpp
@@ -19,10 +19,26 @@ Timer::Timer(const duration& expiry_time):
 Timer::~Timer() {
 }
 
+void Timer::expires_after(const duration& expiry_time) {
+    return alarm_ptr_->update_time(std::chrono::system_clock::now()+expiry_time);
+}
+
+void Timer::expires_at(const time_point& expiry_time) {
+    return alarm_ptr_->update_time(expiry_time);
+}
+
+Timer::time_point Timer::expiry() const {
+    return alarm_ptr_->time;
+}
+
 void Timer::async_wait(std::function<void(const std::error_code&)> callback) {
     return alarm_ptr_->async_wait(std::move(callback));
 }
 
 void Timer::wait() {
     return alarm_ptr_->wait();
+}
+
+void Timer::cancel() {
+    return alarm_ptr_->cancel();
 }
