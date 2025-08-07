d02a8adea21dd95415bd3943e5d7067b68ee50d2
hexu <hexu_1985@sina.com>
2025-08-07 18:37:53 +0800

    update timer/recipe-01/boost

--- timer/recipe-01/asio/timer_period.cpp
+++ timer/recipe-01/asio/timer_period.cpp
@@ -7,26 +7,32 @@
 
 using namespace std::placeholders;
 
-void periodic_task(asio::system_timer& timer, const std::error_code& ec) {
+void periodic_task(asio::system_timer& timer, std::chrono::seconds intervel, const std::error_code& ec);
+
+void start_timer(asio::system_timer& timer, std::chrono::seconds intervel) {
+    // 再次设置定时器以实现周期性执行
+    timer.expires_after(intervel);
+    // also can use
+    // timer.expires_at(timer.expiry() + std::chrono::seconds(2));
+    timer.async_wait([&timer, intervel](const std::error_code& ec) { periodic_task(timer, intervel, ec); });
+}
+
+void periodic_task(asio::system_timer& timer, std::chrono::seconds intervel, const std::error_code& ec) {
     if (ec == asio::error::operation_aborted) {
         print_message("任务取消了");
         return;
     }
 
     print_message("周期性任务执行");
-    // 再次设置定时器以实现周期性执行
-    timer.expires_after(std::chrono::seconds(2));
-    // also can use
-    // timer.expires_at(timer.expiry() + std::chrono::seconds(2));
-    timer.async_wait([&timer](const std::error_code& ec) { periodic_task(timer, ec); });
+    start_timer(timer, intervel);
 }
 
 int main() {
     asio::io_context io;
     
     // 第一次启动定时器
-    asio::system_timer timer(io, std::chrono::seconds(2));
-    timer.async_wait(std::bind(periodic_task, std::ref(timer), _1));
+    asio::system_timer timer(io);
+    start_timer(timer, std::chrono::seconds(2));
 
     print_message("周期性定时器已启动，每2秒执行一次");
 
